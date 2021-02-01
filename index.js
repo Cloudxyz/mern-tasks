861cce10fdb1284595845fd21cdb7e7a5c1a284b
Alan Cordova <info.devalan@gmail.com>
2021-02-01 00:43:07 -0600

    ajustes para deployment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true}));
 
 // puerto de la app
-const port = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
 })
 
 // arrancar la app
-app.listen(port, '', () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Escuchando el puerto ${port}`)
 })
